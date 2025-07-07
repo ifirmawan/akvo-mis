6cca6a13a17097de8ecc299002ae8e3c44591b7d
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-07-07 08:43:29 +0700

    [#75] FE: Refactor data transformation logic in DataDetail and BatchDetail components

--- frontend/src/pages/manage-data/DataDetail.jsx
+++ frontend/src/pages/manage-data/DataDetail.jsx
@@ -8,7 +8,13 @@ import React, {
 import { Table, Button, Space, Spin, Alert } from "antd";
 import { LoadingOutlined, HistoryOutlined } from "@ant-design/icons";
 import { EditableCell } from "../../components";
-import { api, QUESTION_TYPES, store, uiText } from "../../lib";
+import {
+  api,
+  QUESTION_TYPES,
+  store,
+  uiText,
+  transformDetailData,
+} from "../../lib";
 import { useNotification } from "../../util/hooks";
 import { flatten, isEqual } from "lodash";
 import { HistoryTable } from "../../components";
@@ -166,97 +172,12 @@ const DataDetail = ({
       api
         .get(`data/${id}`)
         .then((res) => {
-          // Process the data differently for repeatable and non-repeatable question groups
-          const transformedData = [];
-
-          // Process each question group
-          questionGroups?.forEach((qg) => {
-            if (qg?.repeatable) {
-              // For repeatable groups, we need to find how many instances of each question exist
-              // Group the response data by question ID
-              const questionOccurrences = {};
-              qg.question.forEach((q) => {
-                // Find all responses for this question
-                const responses = res.data.filter((r) => r.question === q.id);
-                // Track the maximum number of responses for any question in this group
-                const count = responses.length;
-                questionOccurrences[q.id] = count;
-              });
-
-              // Find the maximum count of responses for any question in this group
-              const maxCount = Math.max(
-                ...Object.values(questionOccurrences),
-                0
-              );
-
-              // Create copies of the question group for each response instance
-              for (let i = 0; i < maxCount; i++) {
-                const questionGroupCopy = {
-                  ...qg,
-                  label: `${qg.label} #${i + 1}`,
-                };
-                questionGroupCopy.question = qg.question
-                  .filter((q) => {
-                    if (q?.dependency) {
-                      const isValid = q.dependency.some((d) => {
-                        const value = res.data.filter(
-                          (r) => r.question === d.id
-                        )?.[i]?.value;
-                        return validateDependency(d, value);
-                      });
-                      return isValid;
-                    }
-                    return q;
-                  })
-                  .map((q) => {
-                    // Get all responses for this question
-                    const responses = res.data.filter(
-                      (r) => r.question === q.id
-                    );
-
-                    // If the question has a dependency, then get the response
-                    // for the current instance (i) or the first instance (0)
-                    let response = responses?.find((r) => r?.index === i);
-                    if (q?.dependency) {
-                      response = responses?.[i] ||
-                        responses?.[0] || {
-                          value: null,
-                          history: false,
-                        };
-                    }
-                    if (q?.type === QUESTION_TYPES.attachment) {
-                      response = responses.find((r) =>
-                        r.value?.includes(`${q.id}-${i}`)
-                      );
-                    }
-
-                    return {
-                      ...q,
-                      id: i > 0 ? `${q.id}-${i}` : q.id, // Add suffix for duplicate questions
-                      value: response?.value,
-                      history: response?.history || false,
-                    };
-                  });
-                transformedData.push(questionGroupCopy);
-              }
-            } else {
-              // For non-repeatable groups, process as before
-              transformedData.push({
-                ...qg,
-                question: qg.question.flatMap((q) =>
-                  res.data
-                    .filter((r) => r.question === q.id)
-                    .map((d, dx) => ({
-                      ...q,
-                      id: dx ? `${q.id}-${dx}` : q.id,
-                      value: d?.value,
-                      history: d?.history || false,
-                    }))
-                ),
-              });
-            }
-          });
-
+          const transformedData = transformDetailData(
+            res.data,
+            questionGroups,
+            validateDependency,
+            QUESTION_TYPES
+          );
           setDataset(transformedData);
         })
         .catch((e) => {

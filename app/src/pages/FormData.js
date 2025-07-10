e0dcdd43586fd5aa6ae92211f8e279f7fdc80982
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-07-10 20:21:08 +0700

    [#75] Mobile app: Refactor FormDataPage to remove unused sync functionality and simplify data fetching logic

--- app/src/pages/FormData.js
+++ app/src/pages/FormData.js
@@ -1,17 +1,11 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
-import { Button, Dialog, Text } from '@rneui/themed';
-import { View, ActivityIndicator, StyleSheet, ToastAndroid } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
 import moment from 'moment';
-import * as Network from 'expo-network';
-import * as Sentry from '@sentry/react-native';
 import { useSQLiteContext } from 'expo-sqlite';
-import { UserState, UIState, FormState, BuildParamsState } from '../store';
+import { UserState, UIState, FormState } from '../store';
 import { BaseLayout } from '../components';
 import { crudDataPoints } from '../database/crud';
-import { i18n, backgroundTask, api } from '../lib';
+import { i18n } from '../lib';
 import { getCurrentTimestamp } from '../form/lib';
-import crudJobs, { jobStatus } from '../database/crud/crud-jobs';
 
 const convertMinutesToHHMM = (minutes) => {
   const hours = Math.floor(minutes / 60);
@@ -23,35 +17,20 @@ const convertMinutesToHHMM = (minutes) => {
   return `${formattedHours}h ${formattedMinutes}m`;
 };
 
-const SyncButton = ({ onPress, disabled = false }) => (
-  <Button type="clear" disabled={disabled} onPress={onPress} testID="button-to-trigger-sync">
-    <Icon
-      name={disabled ? 'checkmark-done' : 'sync'}
-      color={disabled ? 'dodgerblue' : 'black'}
-      size={18}
-      testID="icon-sync"
-    />
-  </Button>
-);
-
 const FormDataPage = ({ navigation, route }) => {
   const formId = route?.params?.id;
   const showSubmitted = route?.params?.showSubmitted || false;
-  const { lang: activeLang, networkType } = UIState.useState((s) => s);
+  const activeLang = UIState.useState((s) => s.lang);
   const trans = i18n.text(activeLang);
-  const { id: activeUserId, syncWifiOnly } = UserState.useState((s) => s);
+  const { id: activeUserId } = UserState.useState((s) => s);
   const [search, setSearch] = useState(null);
   const [data, setData] = useState([]);
-  const [showConfirmationSyncDialog, setShowConfirmationSyncDialog] = useState(false);
-  const [syncing, setSyncing] = useState(false);
-  const serverURL = BuildParamsState.useState((s) => s.serverURL);
   const db = useSQLiteContext();
 
   const fetchData = useCallback(async () => {
-    const submitted = showSubmitted ? 1 : 0;
     let results = await crudDataPoints.selectDataPointsByFormAndSubmitted(db, {
       form: formId,
-      submitted,
+      submitted: 0,
       user: activeUserId,
     });
     results = results.map((res) => {
@@ -92,19 +71,6 @@ const FormDataPage = ({ navigation, route }) => {
     [data, search],
   );
 
-  const loadNetworkType = useCallback(async () => {
-    const { type: networkTypeService } = await Network.getNetworkStateAsync();
-    if (networkType !== networkTypeService) {
-      UIState.update((s) => {
-        s.networkType = networkTypeService;
-      });
-    }
-  }, [networkType]);
-
-  useEffect(() => {
-    loadNetworkType();
-  }, [loadNetworkType]);
-
   const goToDetails = (id) => {
     const findData = filteredData.find((d) => d.id === id);
     const { json: valuesJSON, name: dataPointName } = findData || {};
@@ -131,51 +97,6 @@ const FormDataPage = ({ navigation, route }) => {
     });
   };
 
-  const enableSyncButton = useMemo(() => data.filter((d) => !d.syncedAt).length > 0, [data]);
-
-  const handleSyncButtonOnPress = () => {
-    setShowConfirmationSyncDialog(true);
-  };
-
-  const runSyncSubmision = async () => {
-    await backgroundTask.syncFormSubmission();
-    await fetchData();
-    UIState.update((s) => {
-      s.isManualSynced = true;
-    });
-    setSyncing(false);
-  };
-
-  const handleOnSync = async () => {
-    try {
-      api.setServerURL(serverURL);
-      setShowConfirmationSyncDialog(false);
-      setData([]);
-      setSyncing(true);
-      const activeJob = await crudJobs.getActiveJob(db);
-      if (activeJob) {
-        /**
-         * Delete the active job while it is still in pending status to prevent duplicate submissions.
-         */
-        if (activeJob.status === jobStatus.PENDING) {
-          await crudJobs.deleteJob(db, activeJob.id);
-          await runSyncSubmision();
-        } else {
-          ToastAndroid.show(trans.autoSyncInProgress, ToastAndroid.LONG);
-          setSyncing(false);
-        }
-      } else {
-        await runSyncSubmision();
-      }
-    } catch (error) {
-      Sentry.captureMessage('[FormData] unable to sync submission manually');
-      Sentry.captureException(error);
-      setData(data);
-      setSyncing(false);
-      ToastAndroid.show(`${error?.errorCode}: ${error?.message}`, ToastAndroid.LONG);
-    }
-  };
-
   const handleOnAction = showSubmitted ? goToDetails : goToEditForm;
 
   return (
@@ -188,50 +109,10 @@ const FormDataPage = ({ navigation, route }) => {
         value: search,
         action: setSearch,
       }}
-      rightComponent={
-        !showSubmitted ||
-        (!filteredData.length && !search) ||
-        (syncWifiOnly && networkType !== Network.NetworkStateType.WIFI) ? (
-          false
-        ) : (
-          <SyncButton disabled={!enableSyncButton} onPress={handleSyncButtonOnPress} />
-        )
-      }
     >
-      {syncing ? (
-        <View style={styles.loadingContainer} testID="sync-loading">
-          <ActivityIndicator />
-        </View>
-      ) : (
-        <BaseLayout.Content data={filteredData} action={handleOnAction} testID="data-point-list" />
-      )}
-
-      {/* confirmation dialog to sync */}
-      <Dialog visible={showConfirmationSyncDialog} testID="sync-confirmation-dialog">
-        <Text testID="sync-confirmation-text">{trans.confirmSync}</Text>
-        <Dialog.Actions>
-          <Dialog.Button
-            title={trans.buttonOk}
-            onPress={handleOnSync}
-            testID="sync-confirmation-ok"
-          />
-          <Dialog.Button
-            title={trans.buttonCancel}
-            onPress={() => setShowConfirmationSyncDialog(false)}
-            testID="sync-confirmation-cancel"
-          />
-        </Dialog.Actions>
-      </Dialog>
+      <BaseLayout.Content data={filteredData} action={handleOnAction} testID="data-point-list" />
     </BaseLayout>
   );
 };
-
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-});
 
 export default FormDataPage;

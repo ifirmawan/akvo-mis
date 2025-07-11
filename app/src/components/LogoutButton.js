2b1c147020b49f36eecc00c7fa647b4e298850c6
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-07-11 11:42:14 +0700

    [#75] Mobile app: Remove unused database connection logic in LogoutButton component

--- app/src/components/LogoutButton.js
+++ app/src/components/LogoutButton.js
@@ -5,7 +5,6 @@ import { useNavigation } from '@react-navigation/native';
 import * as SQLite from 'expo-sqlite';
 import { AuthState, UserState, FormState, UIState } from '../store';
 import { api, cascades, i18n } from '../lib';
-import { DATABASE_NAME } from '../lib/constants';
 import sql from '../database/sql';
 
 const LogoutButton = () => {
@@ -14,15 +13,13 @@ const LogoutButton = () => {
   const navigation = useNavigation();
   const activeLang = UIState.useState((s) => s.lang);
   const trans = i18n.text(activeLang);
+  const db = SQLite.useSQLiteContext();
 
   const handleNoPress = () => {
     setVisible(false);
   };
 
   const handleYesPress = async () => {
-    const db = await SQLite.openDatabaseAsync(DATABASE_NAME, {
-      useNewConnection: true,
-    });
     const tables = ['sessions', 'users', 'forms', 'config', 'datapoints', 'jobs'];
     await Promise.all(
       tables.map(async (table) => {

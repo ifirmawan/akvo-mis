dd6d155265920133ed3447542ff4655c0de4d094
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-07-23 09:53:29 +0700

    [#89] v1_forms: Preserve user form assignments during reset process

--- backend/api/v1/v1_forms/tests/tests_reset_forms.py
+++ backend/api/v1/v1_forms/tests/tests_reset_forms.py
@@ -3,10 +3,12 @@ from django.test import TestCase
 from django.test.utils import override_settings
 from django.core.management import call_command
 from api.v1.v1_forms.models import Forms
+from api.v1.v1_profile.models import Administration
+from api.v1.v1_profile.tests.mixins import ProfileTestHelperMixin
 
 
 @override_settings(USE_TZ=False)
-class FormSeederTestCase(TestCase):
+class FormSeederTestCase(TestCase, ProfileTestHelperMixin):
     def setUp(self):
         # Run the form seeder twice to ensure forms
         # are populated with the last version is 2
@@ -71,3 +73,38 @@ class FormSeederTestCase(TestCase):
                 reset_forms_dict,
                 f"Form {form_id} was deleted during reset."
             )
+
+    def test_call_reset_forms_with_users_form_assignment(self):
+        call_command("administration_seeder", "--test", 1)
+        # Create a user and assign a form to them
+        adm = Administration.objects.filter(
+            level__level=2
+        ).order_by("?").first()
+        form = Forms.objects.get(pk=1)
+        user = self.create_user(
+            email="user@example.com",
+            role_level=self.IS_ADMIN,
+            administration=adm,
+            form=form,
+        )
+        total_before = user.user_form.count()
+        output = self.call_command("--test")
+        self.assertIn("Successfully reset all forms.", output)
+        user.refresh_from_db()
+
+        total_after = user.user_form.count()
+
+        self.assertEqual(
+            total_before,
+            total_after,
+            "User's form assignments changed after reset."
+        )
+
+        # Check if the user has the form assigned after reset
+        self.assertIn(
+            form,
+            [
+                uf.form for uf in user.user_form.all()
+            ],
+            "User does not have the form assigned after reset."
+        )

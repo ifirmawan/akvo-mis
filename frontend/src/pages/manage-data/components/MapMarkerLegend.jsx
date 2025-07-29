9b901a848f7b10695be287b0f031f0273cfd87ee
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-07-29 17:38:22 +0700

    [#95] FE: Add MapMarkerLegend component for displaying legend items on the map

---
+++ frontend/src/pages/manage-data/components/MapMarkerLegend.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Space } from "antd";
+
+const MapMarkerLegend = ({ title = "", options = [], onClick = () => {} }) => {
+  const [activeOption, setActiveOption] = useState(null);
+
+  const handleOnClick = (option) => {
+    setActiveOption(option);
+    onClick(option);
+  };
+
+  return (
+    <div className="marker-legend">
+      <h4>{title}</h4>
+      <Space
+        direction="horizontal"
+        align="center"
+        wrap={true}
+        size={[16, 0]}
+        style={{ justifyContent: "center" }}
+      >
+        {options.map((sO, sI) => (
+          <div
+            key={sI}
+            className="legend-item"
+            onClick={() => handleOnClick(sO)}
+          >
+            <Space direction="horizontal" align="top">
+              <div
+                className="circle-legend"
+                style={{ backgroundColor: sO?.color }}
+              />
+              <span
+                style={{
+                  fontWeight: activeOption?.id === sO.id ? "600" : "400",
+                }}
+              >
+                {sO?.label}
+              </span>
+            </Space>
+          </div>
+        ))}
+      </Space>
+    </div>
+  );
+};
+
+export default MapMarkerLegend;

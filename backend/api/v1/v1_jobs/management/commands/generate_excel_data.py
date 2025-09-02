568a51a53bc46c8e29bef0356e7e37bc4c0488ee
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-09-02 14:03:24 +0700

    [#126] v1_jobs: Simplify argument handling and improve error messages commands
    Simplify argument handling by removing unnecessary download_type and use_label and improve error messages in generate_excel_data and job_download commands

--- backend/api/v1/v1_jobs/management/commands/generate_excel_data.py
+++ backend/api/v1/v1_jobs/management/commands/generate_excel_data.py
@@ -12,53 +12,34 @@ CRONJOB_RESULT_DIR = "cronjob_results"
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("form_id", nargs="?", type=int)
-        parser.add_argument(
-            "--submission", "-s", nargs="?", default=None, type=str
-        )
-        parser.add_argument(
-            "--latest", "-l", nargs="?", default=False, type=bool
-        )
-        parser.add_argument(
-            "--use-label", "-lb", nargs="?", default=False, type=bool
-        )
 
     def handle(self, *args, **options):
-        use_label = options.get("use_label")
-        download_type = "all" if not options.get("latest") else "recent"
         form_id = options.get("form_id")
         if not form_id:
-            print("Please provide form id")
-            exit()
+            self.stdout.write(
+                self.style.ERROR("Form id is required")
+            )
+            return
         form = Forms.objects.get(pk=form_id)
+        if form.parent:
+            self.stdout.write(
+                self.style.ERROR("Please use form registration id")
+            )
+            return
         form_name = form.name.replace(" ", "_").lower()
         process_file = f"process-{form_name}.xlsx"
         writer = pd.ExcelWriter(process_file, engine="xlsxwriter")
         generate_data_sheet(
             writer=writer,
             form=form,
-            administration_ids=None,
-            download_type=download_type,
-            use_label=use_label,
+            child_form_ids=list(form.children.values_list("id", flat=True)),
         )
         writer.save()
 
-        out_file = "-".join(
-            list(
-                filter(
-                    lambda x: x,
-                    [
-                        form_name,
-                        options.get("submission") or "routine",
-                        "latest" if options.get("latest") else None,
-                    ],
-                )
-            )
-        )
-
-        out_file = f"{out_file}.xlsx"
+        out_file = f"{form_name}.xlsx"
 
         url = upload(
             file=process_file, folder=CRONJOB_RESULT_DIR, filename=out_file
         )
-        print(f"File uploaded to {url}")
+        self.stdout.write(self.style.SUCCESS(f"File uploaded to {url}"))
         os.remove(process_file)
--- backend/api/v1/v1_jobs/management/commands/job_download.py
+++ backend/api/v1/v1_jobs/management/commands/job_download.py
@@ -17,26 +17,41 @@ class Command(BaseCommand):
             "-a", "--administration", nargs="?", default=0, type=int
         )
         parser.add_argument(
-            "-t", "--type", nargs="?", default="all", type=str
-        )
-        parser.add_argument(
-            "-l", "--use_label", nargs="?", default=0, type=int
+            "-c", "--child_form_ids", nargs="*", default=[], type=int
         )
 
     def handle(self, *args, **options):
         administration = options.get("administration")
-        arg_type = options.get("type")
-        use_label = options.get("use_label")
-        download_type = "all"
-        if arg_type:
-            download_type = arg_type
+        form_id = options.get("form")[0]
+        form = Forms.objects.get(pk=form_id)
+        # validate form should have parent is null
+        if form.parent is not None:
+            self.stdout.write(
+                self.style.ERROR(
+                    "Form id {0} is not a registration form".format(form.id)
+                )
+            )
+            return
+
+        child_form_ids = options.get("child_form_ids", [])
+        # validate child_form_ids is a child of form
+        if len(child_form_ids):
+            valid_child_form_ids = form.children.values_list("id", flat=True)
+            for child_form_id in child_form_ids:
+                if child_form_id not in valid_child_form_ids:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            "{0} is not a child of form id {1}".format(
+                                child_form_id, form.id
+                            )
+                        )
+                    )
+                    return
         info = {
-            "form_id": options.get("form")[0],
+            "form_id": form_id,
             "administration": administration if administration > 0 else None,
-            "download_type": download_type,
-            "use_label": True if use_label else False,
+            "child_form_ids": child_form_ids,
         }
-        form = Forms.objects.get(pk=options.get("form")[0])
         form_name = form.name.replace(" ", "_").lower()
         today = timezone.datetime.today().strftime("%y%m%d")
         out_file = "download-{0}-{1}-{2}.xlsx".format(
